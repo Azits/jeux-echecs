3336dff6765dcd1482659ffa74b83c1dbc7e7487
Abdoul-Anziz window <cheikhaliabdoulanziz13@gmail.com>
2025-04-09 19:32:51 +0200

    finalisation validation coup si le roi n'est pas en echec

--- src/modele/jeu/JeuxEchecs.java
+++ src/modele/jeu/JeuxEchecs.java
@@ -9,9 +9,31 @@ public class JeuxEchecs extends Jeu{
     }
     public boolean coupValide(Case caseClic1, Case caseClic2) {
         boolean valide = false;
-        ArrayList<Case>caseAccecible=caseClic1.getPiece().getCasesAccessibles();
-        if (caseAccecible.contains(caseClic2)) {
-            valide=true;
+        ArrayList<Case> caseAccessible = caseClic1.getPiece().getCasesAccessibles();
+        if (caseAccessible.contains(caseClic2)) {
+            Case dep = caseClic1;
+            Case arr = caseClic2;
+
+            Piece piece = dep.getPiece();
+            Piece pieceCapturee = arr.getPiece();
+
+            // Simuler le coup
+            dep.quitterLaCase();
+            arr.setPiece(piece);
+            piece.allerSurCase(arr);
+
+            // Valider si pas en échec
+            if (!enEchec(piece.getCouleur())) {
+                valide = true;
+            }
+
+            // Annuler la simulation
+            arr.setPiece(pieceCapturee);
+            dep.setPiece(piece);
+            piece.allerSurCase(dep);
+            if (pieceCapturee != null) {
+                pieceCapturee.allerSurCase(arr);
+            }
         }
         return valide;
     }
